d3ac3e7310b42138226d1a1e9c244000d11ffc35
tom08 <kroll001@live.missouristate.edu>
2017-05-04 19:05:52 -0500

    Added basic method to shuffle a cube

--- multiCubeSolver.py
+++ multiCubeSolver.py
@@ -4,21 +4,27 @@
 
 #NOTE: beginner3Layer tries to write to solutionsLog.txt
 #       We'll have to deal with that in parallellizing. (forgive my spelling)
+import random
 
 from dependencies.solutions import beginner3Layer
 from dependencies.cube import Cube
 
 
+def shuffle_cube(cube):
+    num_shuffles = int(1000*random.random())
+    move_keys = ["U", "D", "L", "R", "F", "B", "U'", "D'", "L'", "R'", "F'", "B'"]
+    for i in range(num_shuffles):
+        cube.state.rotate(cube.state.rotationInfo(random.choice(move_keys)))
+    return cube
+
+
 def main():
     cube = Cube(None, None, None)
 
-# TODO: Figure out the format/object to pass to setConfig.
-#       setConfig converts colors to the internal state the solver expects.
-#       If we can figure out the input, we can move forward with random and
-#       custom inputs.
-    cube.setConfig(None)
-    print cube
-    print cube.getSolution()
+    cube = shuffle_cube(cube)
+    solution = cube.getSolution()
+    for step in reversed(solution):
+        print step[0], ": ", step[1]
 
 
 if __name__ == "__main__":
