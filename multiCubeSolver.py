0555e2ab3b08c43a60503c7b98c6f95e06821e0f
Luke Seidl <lukeseidl@Lukes-MacBook-Pro.local>
2017-05-05 13:31:21 -0500

    Fixed writing to file

--- multiCubeSolver.py
+++ multiCubeSolver.py
@@ -1,32 +1,40 @@
 # Missouri State University
 # CSC 338 Semester Project
 
-
-#NOTE: beginner3Layer tries to write to solutionsLog.txt
-#       We'll have to deal with that in parallellizing. (forgive my spelling)
 import random
+import time
+import sys
+import Queue
 
 from dependencies.solutions import beginner3Layer
 from dependencies.cube import Cube
 
+solution_file = open("solutions_serial.txt", 'w')
+scramble_file = open("scrambles_serial.txt",'w') 
 
-def shuffle_cube(cube):
-    num_shuffles = int(1000*random.random())
-    move_keys = ["U", "D", "L", "R", "F", "B", "U'", "D'", "L'", "R'", "F'", "B'"]
-    for i in range(num_shuffles):
-        cube.state.rotate(cube.state.rotationInfo(random.choice(move_keys)))
-    return cube
+def scramble_and_solve():
+	cube = Cube(None, None, None)
+	num_shuffles = int(1000*random.random())
+	move_keys = ["U", "D", "L", "R", "F", "B", "U'", "D'", "L'", "R'", "F'", "B'"]
+	for i in range(num_shuffles):
+		cube.state.rotate(cube.state.rotationInfo(random.choice(move_keys)))
+	solution = cube.getSolution()
+	solution_moves = []
+	for step in reversed(solution):
+		solution_moves.append(step[0])
+	solution_file.write(str(solution_moves))
+	scramble_file.write(str(cube.state))
 
+def main(num_cubes):
+	scramble_list = []
+	solution_list = []
+	for i in range(num_cubes):
+	    scramble_and_solve()
+	solution_file.close()
+	scramble_file.close()
 
-def main():
-    cube = Cube(None, None, None)
-    cube = shuffle_cube(cube)
-    print cube.state
-    solution = cube.getSolution()
-    for step in reversed(solution):
-        print step[0],
-    
-
-
+start = time.time()
 if __name__ == "__main__":
-    main()
+	num_cubes = int(sys.argv[1])
+	main(num_cubes)
+	print "Total time: ", time.time()-start
--- multiCubeSolverParallel.py
+++ multiCubeSolverParallel.py
@@ -1,57 +1,67 @@
 # Missouri State University
 # CSC 338 Semester Project
+
+# To Run: python multiCubeSolverParallel.py <num_cubes> <num_processes>
 import random
 import sys
 import multiprocessing as mp
-import Queue
 from dependencies.solutions import beginner3Layer
 from dependencies.cube import Cube
-import csv
+import time
+import os
 
 que = mp.Queue()
+
     
-def scramble_and_solve(num_cubes,que):
-        print "called"
-	cube = Cube(None, None, None)
-	num_shuffles = int(1000*random.random())
-	move_keys = ["U", "D", "L", "R", "F", "B", "U'", "D'", "L'", "R'", "F'", "B'"]
-        for i in range(num_shuffles):
-                cube.state.rotate(cube.state.rotationInfo(random.choice(move_keys)))
-	solution = cube.getSolution()
-	solution_moves = []
-	for step in reversed(solution):
-		solution_moves.append(step[0])
-	que.put([cube.state,solution_moves])
+def scramble_and_solve(num_cubes, que, process_num):
+	with open("solutions" + process_num + ".txt", 'w') as solution_file:
+		with open("scramble" + process_num + ".txt", 'w') as scramble_file:
+			for i in range(num_cubes):
+				cube = Cube(None, None, None)
+				num_shuffles = int(1000*random.random())
+				move_keys = ["U", "D", "L", "R", "F", "B", "U'", "D'", "L'", "R'", "F'", "B'"]
+				for j in range(num_shuffles):
+					cube.state.rotate(cube.state.rotationInfo(random.choice(move_keys)))
+				solution = cube.getSolution()
+				solution_moves = []
+				for step in reversed(solution):
+					solution_moves.append(step[0])
+				solution_file.write(str(solution_moves))
+				scramble_file.write(str(cube.state))
+	que.put(("solutions" + process_num + ".txt", "scramble" + process_num + ".txt"))
 
 def main(num_cubes, num_processes):
 	cubes_per_process = num_cubes // num_processes
 	extras = num_cubes % num_processes
-	
 	processes = []
 	for i in range(num_processes):
 		process_num_cubes = cubes_per_process
 		if extras > 0:
 			process_num_cubes += 1
 			extras -= 1
-		p = mp.Process(target = scramble_and_solve, args = [process_num_cubes, que])
+		p = mp.Process(target = scramble_and_solve, args = [process_num_cubes, que, str(i)])
 		processes.append(p)
 		p.start()
 	for p in processes:
 		p.join()
-		
-	scramble_list = []
-	solution_list = []
-        solution_file = open("solutions.txt", 'w')
-        scramble_file = open("scrambles.txt",'w')
+	
+	with open('solutions.txt', 'w') as solution_outfile:
+		with open('scrambles.txt', 'w') as scramble_outfile:
+			while not que.empty():
+				pair = que.get()
+				with open(pair[0],'r') as infile:
+					for line in infile:
+						solution_outfile.write(line)
+				with open(pair[1],'r') as infile:
+					for line in infile:
+						scramble_outfile.write(line)
+				os.remove(pair[0])
+				os.remove(pair[1])
+					
 
-	while not que.empty():
-                pair = que.get()
-                solution_file.write(str(pair[1]))
-                scramble_file.write(str(pair[0]))
-        solution_file.close()
-        scramble_file.close()
-
+start = time.time()
 if __name__ == "__main__":
 	num_cubes = int(sys.argv[1])
 	num_processes = int(sys.argv[2])
 	main(num_cubes, num_processes)
+	print "Total time: ", time.time()-start
